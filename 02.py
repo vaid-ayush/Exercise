0fc73bda43494c9b890cb7bb5ebc030363e69e7f
ayush <ayush.vaid@shorthillstech.com>
2022-08-04 14:40:57 +0530

    Exercise problem

---
+++ 02.py
@@ -0,0 +1,10 @@
+def addition(n):
+    j=0
+    for i in range(n):
+
+        print("First number is:", i)
+        print("second number is:", j)
+        print("sum is", int(i + j))
+        j=i
+
+addition(10)
