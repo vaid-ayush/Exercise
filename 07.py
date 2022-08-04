0066a3c97ccb20d3c8860617ac1f4740e955b150
ayush <ayush.vaid@shorthillstech.com>
2022-08-04 15:17:39 +0530

    count in a string

---
+++ 07.py
@@ -0,0 +1,7 @@
+# Return the count of a given substring from a string
+
+def name(string,name):
+    x=string.count(name)
+    print({name},"appeared",{x},"times")
+
+name("Ayush Ayush Ayush Ayush vaid","Ayush")
