3ae10ef73b1924ac8e5235b5d27f2c92087146cd
ayush <ayush.vaid@shorthillstech.com>
2022-08-05 18:00:30 +0530

    format strings

---
+++ 2_08.py
@@ -0,0 +1,7 @@
+# Format variables using a string.format() method.
+
+TotalMoney = 1000
+quantity = 3
+price = 450
+
+print("I have {0} dollars so I can buy {1} footballs for {2:.2f} dollars".format(TotalMoney,quantity,price))
