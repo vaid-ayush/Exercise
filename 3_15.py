c47ac757d5c5ccd89e31550af25691163ee7d0d4
ayush <ayush.vaid@shorthillstech.com>
2022-08-06 20:12:37 +0530

    Use a loop to display elements from a given list present at odd index positions

---
+++ 3_15.py
@@ -0,0 +1,8 @@
+# Use a loop to display elements from a given list present at odd index positions
+
+l1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+l = len(l1)
+print(l)
+for i in range(1,l,2):
+    print(l1[i])
+    
